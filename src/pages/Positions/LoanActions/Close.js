930a798ffd5b3a1745e154274941800d51271d1b
vbstreetz <vbstreetz@gmail.com>
2021-02-03 17:41:38 +0300

    fix: approve debt asset before closing loan

--- src/pages/Positions/LoanActions/Close.js
+++ src/pages/Positions/LoanActions/Close.js
@@ -1,14 +1,70 @@
 import React from 'react';
+import * as ethers from 'ethers';
 import { Box, Button, DialogTitle, DialogContent } from '@material-ui/core';
 import { useWallet } from 'contexts/wallet';
 import { useNotifications } from 'contexts/notifications';
 import { useStyles, CloseButton } from './utils';
+import ERC20_CONTRACT_ABI from 'abis/erc20.json';
+import { formatUnits } from 'utils/big-number';
 
-export default function({ loan, closeModal }) {
+export default function({ loan, collateralName, debtName, closeModal }) {
   const classes = useStyles();
   const [isWorking, setIsWorking] = React.useState(false);
-  const { collateralContracts } = useWallet();
+  const {
+    collateralContracts,
+    address,
+    config: { tokens },
+    signer,
+  } = useWallet();
   const { tx } = useNotifications();
+  const [isApproved, setIsApproved] = React.useState(false);
+
+  const [collateralDecimals] = tokens[collateralName];
+  const [debtDecimals, debtAddress] = tokens[debtName];
+  const debtAmount = loan.amount;
+
+  const debtContract = React.useMemo(
+    () =>
+      signer &&
+      debtAddress &&
+      new ethers.Contract(debtAddress, ERC20_CONTRACT_ABI, signer),
+    [debtAddress, signer]
+  );
+
+  const multiDebtContract = collateralContracts[loan.type];
+  const multiDebtAddress = multiDebtContract.address;
+
+  React.useEffect(() => {
+    let isMounted = true;
+    (async () => {
+      if (!(debtContract && multiDebtAddress && address)) {
+        return setIsApproved(true);
+      }
+      const allowance = await debtContract.allowance(address, multiDebtAddress);
+      if (isMounted) setIsApproved(allowance.gte(debtAmount));
+    })();
+    return () => (isMounted = false);
+  }, [debtContract, address, multiDebtAddress, debtAmount]);
+
+  const onApproveOrClose = async e => {
+    e.preventDefault();
+    !isApproved ? approve() : close();
+  };
+
+  const approve = async () => {
+    try {
+      setIsWorking('Approving...');
+      await tx(`Approving ${debtName}`, `Approved ${debtName}`, () =>
+        debtContract.approve(multiDebtAddress, debtAmount)
+      );
+
+      if (!(signer && multiDebtAddress && address)) return setIsApproved(true);
+      const allowance = await debtContract.allowance(address, multiDebtAddress);
+      setIsApproved(allowance.gte(debtAmount));
+    } finally {
+      setIsWorking(false);
+    }
+  };
 
   const close = async () => {
     try {
@@ -31,13 +87,18 @@ export default function({ loan, closeModal }) {
       </DialogTitle>
       <DialogContent className={classes.body}>
         <Box mb={2}>
+          You are paying {formatUnits(loan.amount, debtDecimals)} {debtName} to
+          get back {formatUnits(loan.collateral, collateralDecimals)}{' '}
+          {collateralName}.
+        </Box>
+        <Box mb={2}>
           <Button
             color="secondary"
             variant="outlined"
-            onClick={close}
+            onClick={onApproveOrClose}
             disabled={!!isWorking}
           >
-            {isWorking ? isWorking : 'CLOSE'}
+            {isWorking ? isWorking : !isApproved ? 'Approve' : 'CLOSE LOAN'}
           </Button>
         </Box>
       </DialogContent>
